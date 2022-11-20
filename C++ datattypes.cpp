ef59780f747f1521e12b59600e47fc68f2ff4666
iamsoankit <98892759+iamsoankit@users.noreply.github.com>
2022-11-20 20:25:58 +0530

    Add files via upload

---
+++ C++ datattypes.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+int main() 
+{ 
+    int a=8;
+    cout<<a<<endl;
+    
+    char c='u';
+    cout<<c<<endl;
+
+    bool b1=true;
+    bool b2=false;
+    cout<<b1<<endl;
+    cout<<b2<<endl;
+
+    float s1=78.99;
+    float s2=8.6;
+    double s3=9.2;
+    cout<<s1<<endl;
+    cout<<s2<<endl;
+    cout<<s3<<endl;
+
+}
+
